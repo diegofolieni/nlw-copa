10e1aa4b7cea623ad1e5b935efc42eadd00ccf49
Diego Folieni <dfoliene@gmail.com>
2022-11-04 21:43:32 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,56 @@
+//Usar String com `` permite fazer quebra de linha
+//Usar String com `` permite fazer interpolação de String com ${}
+//`` é chamado de templat literals
+
+function createGame(player1, hour, player2){
+    return `
+        <li>
+            <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
+            <strong>${hour}</strong>                                                      
+            <img src="./assets/icon-${player2}.svg" alt="Banderia da ${player2}" />
+        </li>    
+    `
+
+}
+
+let delay = -0.4;
+function createCard(date, day, games){
+    delay = delay + 0.4;
+    return `
+        <div class="card" style="animation-delay: ${delay}">
+            <h2>${date} <span>${day}</span></h2>
+            <ul>
+                ${games}                                               
+            </ul>
+        </div>    
+    `
+}
+
+document.querySelector("#cards").innerHTML =
+    createCard("20/11", "domingo",
+        createGame("qatar","13:00","ecuador"))+
+    createCard("21/11", "segunda",
+        createGame("england", "10:00", "iran")+
+        createGame("senegal", "13:00", "netherlands")+
+        createGame("usa", "16:00", "wales"))+
+    createCard("22/11", "terça",
+        createGame("argentina", "07:00", "saudi-arabia")+
+        createGame("denmark", "10:00", "tunisia")+
+        createGame("mexico", "13:00", "poland")+
+        createGame("france", "16:00", "australia"))+
+    createCard("23/11", "quarta",
+        createGame("marocco", "07:00", "croatia")+
+        createGame("germany", "10:00", "japan")+
+        createGame("spain"  , "13:00", "costa-rica")+
+        createGame("belgium", "16:00", "canada"))+
+    createCard("24/11", "quinta", 
+        createGame("switzerland","07:00","cameroon") +
+        createGame("uruguay", "10:00", "south-korea")+
+        createGame("portugal"   ,"13:00","ghana")    +
+        createGame("brazil"     ,"16:00","serbia")) +
+    createCard("25/11", "sexta",
+        createGame("wales", "07:00", "iran")+
+        createGame("qatar", "10:00", "senegal")+
+        createGame("netherlands", "13:00", "ecuador")+
+        createGame("england", "16:00", "usa")
+        )
